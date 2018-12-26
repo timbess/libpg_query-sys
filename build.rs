d404205f05ec4bfbbc2f74d0de379abc907664ab
Timothy Bess <tdbgamer@gmail.com>
2018-12-25 22:31:35 -0500

    initialize repo

---
+++ build.rs
@@ -0,0 +1,26 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    println!("cargo:rustc-link-search=native={}", "./c_libs/libpg_query");
+    println!("cargo:rustc-link-lib=static={}", "pg_query");
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("./c_libs/libpg_query/pg_query.h")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ default.nix
@@ -0,0 +1,51 @@
+let
+
+moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+stableRust = nixpkgs.latest.rustChannels.stable.rust;
+stableCargo = nixpkgs.latest.rustChannels.stable.cargo;
+buildRustPackage = nixpkgs.callPackage (import <nixpkgs/pkgs/build-support/rust>) {
+  rust = {
+    rustc = stableRust;
+    cargo = stableCargo;
+  };
+};
+
+derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bindgen, ... }:
+  buildRustPackage rec {
+  name = "libpg_query-sys";
+  version = "0.1.0";
+
+  src = ./.;
+
+  libclang = llvmPackages_39.libclang.lib;
+  clang = llvmPackages_39.clang;
+
+  buildInputs = [ libclang rust-bindgen ];
+  propagatedBuildInputs = [ clang ];
+
+  configurePhase = ''
+    export LIBCLANG_PATH=${libclang}/lib
+  '';
+
+  cargoSha256 = "0rrp5vgjyv02lljr06fr8zxzn1syqhfjcch7v38y4xq2p39j6znp";
+
+  meta = with stdenv.lib; {
+    description = "C library for accessing the PostgreSQL parser outside of the server.";
+    homepage = https://github.com/lfittl/libpg_query;
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ lfittl ];
+  };
+
+  shellHook = ''
+    export LIBCLANG_PATH=${libclang}/lib
+  '';
+};
+
+in
+
+  nixpkgs.callPackage derivation {
+    inherit buildRustPackage;
+    rust = stableRust;
+    cargo = stableCargo;
+  }
