cf9403c65ae7b61f594fd91fb756adbb5ea8cfe4
Timothy Bess <tdbgamer@gmail.com>
2018-12-25 23:29:30 -0500

    fixing build.rs

--- build.rs
+++ build.rs
@@ -6,21 +6,21 @@ use std::path::PathBuf;
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    Command::new(format!("cp -r ./c_libs/libpg_query {}/.", out_path))
+    Command::new(format!("cp -r ./c_libs/libpg_query {}", out_path.display()))
         .output()
-        .expect("Failed to copy libpg_query to OUT_DIR {}", out_path);
+        .expect("Failed to copy libpg_query to OUT_DIR {}", out_path.display());
 
     Command::new("make")
-        .current_dir(format!("{}/libpg_query", out_path))
+        .current_dir(format!("{}/libpg_query", out_path.display()))
         .output()
         .expect("Failed to build libpg_query");
 
-    println!("cargo:rustc-link-search=native={}", "./c_libs/libpg_query");
+    println!("cargo:rustc-link-search=native={}", out_path.join("libpg_query").display());
     println!("cargo:rustc-link-lib=static={}", "pg_query");
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("./c_libs/libpg_query/pg_query.h")
+        .header(out_path.join("libpg_query/pg_query.h"))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
