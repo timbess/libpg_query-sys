76d0d1d8bb32473467cc9b5bbf3f73c5dd83e14f
Timothy Bess <tdbgamer@gmail.com>
2018-12-25 23:19:32 -0500

    run makefile

--- build.rs
+++ build.rs
@@ -5,8 +5,13 @@ use std::process::Command;
 use std::path::PathBuf;
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    Command::new(format!("cp -r ./c_libs/libpg_query {}/.", out_path))
+        .output()
+        .expect("Failed to copy libpg_query to OUT_DIR {}", out_path);
+
     Command::new("make")
-        .current_dir("./c_libs/libpg_query")
+        .current_dir(format!("{}/libpg_query", out_path))
         .output()
         .expect("Failed to build libpg_query");
 
@@ -21,8 +26,6 @@ fn main() {
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
