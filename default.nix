08fd27a069f1c3c966e5e717c6257da5d7fac4d7
Timothy Bess <tdbgamer@gmail.com>
2018-12-26 13:35:33 -0500

    fix nix build

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ buildRustPackage = nixpkgs.callPackage (import <nixpkgs/pkgs/build-support/rust>
   };
 };
 
-derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bindgen, ... }:
+derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bindgen, binutils, gnumake, ... }:
   buildRustPackage rec {
   name = "libpg_query-sys";
   version = "0.1.0";
@@ -21,14 +21,14 @@ derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bin
   libclang = llvmPackages_39.libclang.lib;
   clang = llvmPackages_39.clang;
 
-  buildInputs = [ libclang rust-bindgen ];
+  buildInputs = [ libclang rust-bindgen gnumake binutils ];
   propagatedBuildInputs = [ clang ];
 
   configurePhase = ''
     export LIBCLANG_PATH=${libclang}/lib
   '';
 
-  cargoSha256 = "0rrp5vgjyv02lljr06fr8zxzn1syqhfjcch7v38y4xq2p39j6znp";
+  cargoSha256 = "0g87vcvaih4g3i2n6ac0xaszdf774k8n1yw5hn64mcpi3dwxsz0n";
 
   meta = with stdenv.lib; {
     description = "C library for accessing the PostgreSQL parser outside of the server.";
