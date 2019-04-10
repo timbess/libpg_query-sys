1979ee9cf1b3bf77201bd18d36e1358a9e67e3c4
Timothy Bess <tdbgamer@gmail.com>
2019-04-10 11:33:01 -0400

    cleanup default.nix

--- default.nix
+++ default.nix
@@ -24,11 +24,9 @@ derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bin
   buildInputs = [ libclang rust-bindgen gnumake binutils ];
   propagatedBuildInputs = [ clang ];
 
-  configurePhase = ''
-    export LIBCLANG_PATH=${libclang}/lib
-  '';
+  LIBCLANG_PATH="${libclang}/lib";
 
-  cargoSha256 = "0g87vcvaih4g3i2n6ac0xaszdf774k8n1yw5hn64mcpi3dwxsz0n";
+  cargoSha256 = "0rrp5vgjyv02lljr06fr8zxzn1syqhfjcch7v38y4xq2p39j6znp";
 
   meta = with stdenv.lib; {
     description = "C library for accessing the PostgreSQL parser outside of the server.";
@@ -36,10 +34,6 @@ derivation = { stdenv, rustPlatform, buildRustPackage, llvmPackages_39, rust-bin
     license = licenses.bsd3;
     maintainers = with maintainers; [ lfittl ];
   };
-
-  shellHook = ''
-    export LIBCLANG_PATH=${libclang}/lib
-  '';
 };
 
 in
